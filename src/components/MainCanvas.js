35cd6d9aaf1bf419cdedca9f74a1110c97e28c62
Ian Munro <ianmunrobot@gmail.com>
2017-01-25 14:39:42 -0500

    added build scripts
    also added
    p5
    volleyball logging

---
+++ src/components/MainCanvas.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import P5Wrapper from 'react-p5-wrapper';
+
+export default () => {
+  return (
+    <div id="mainCanvas">
+      <P5Wrapper sketch={sketch} />
+    </div>
+  )
+}
+
+function sketch (p) {
+  let rotation = 0;
+
+  p.setup = function () {
+    p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
+  };
+
+  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    if (props.rotation){
+      rotation = props.rotation * Math.PI / 180;
+    }
+  };
+
+  p.draw = function () {
+    p.background(100);
+    p.noStroke();
+    p.push();
+    p.rotateY(rotation);
+    p.box(100);
+    p.pop();
+  };
+};
