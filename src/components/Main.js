f8a21763d6332d9b78cf7cd3a410eeacfc5f6c5c
David Rhodes <rhodesdav@gmail.com>
2017-01-26 18:43:35 -0500

    closes #41 with refactoring

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,69 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {connect} from 'react-redux';
 import RightMenu from './RightMenu';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import ShapeSettings from './ShapeSettings';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import FontIcon from 'material-ui/FontIcon';
 import {red500, yellow500, blue500} from 'material-ui/styles/colors';
 import MainCanvas from './MainCanvas';
-
 import {togglePlay} from '../redux/play';
-
 // Our root component
-
 injectTapEventPlugin();
-
 const styles = {
-	icon: {
-		marginRight: 24
-	},
-	buttonIcon: {
-		fontSize: 50
-	},
-	button: {
-		position: 'absolute',
-		left: 15,
-		bottom: 15
-	},
-	canvas: {
-		margin: 0,
-	  	display: 'flex',
-
-	  /* This centers our sketch horizontally. */
-	  justifyContent: 'center',
-
-	  /* This centers our sketch vertically. */
-	  alignItems: 'center'
-	}
+  icon: {
+    marginRight: 24
+  },
+  buttonIcon: {
+    fontSize: 50
+  },
+  button: {
+    position: 'absolute',
+    left: 15,
+    bottom: 15
+  },
+  canvas: {
+    margin: 0,
+    display: 'flex',
+    /* This centers our sketch horizontally. */
+    justifyContent: 'center',
+    /* This centers our sketch vertically. */
+    alignItems: 'center'
+  }
 };
-
-
 const Main = (props) => (
-
   <div id="outer-container">
     <main id="page-wrap">
-      <RightMenu />
-      <MainCanvas />
-<ShapeSettings />
-  		<FloatingActionButton style={styles.button} color={blue500}>
-  			<FontIcon onClick={() => {
-            props.togglePlay(props.isPlaying)
-          }} style={styles.buttonIcon} className="material-icons">play_circle_outline</FontIcon>
-  		</FloatingActionButton>
+      <RightMenu/>
+      <MainCanvas/>
+      <ShapeSettings/>
+      <FloatingActionButton style={styles.button} color={blue500}>
+        <FontIcon onClick={() => {
+          props.togglePlay(props.isPlaying)
+        }} style={styles.buttonIcon} className="material-icons">{props.isPlaying
+            ? 'pause_circle_outline'
+            : 'play_circle_outline'}
+        </FontIcon>
+      </FloatingActionButton>
     </main>
   </div>
-
-
 );
-
 const mapStateToProps = (state) => {
-  return {
-    isPlaying: state.isPlaying
-  };
+  return {isPlaying: state.isPlaying};
 };
-
 const mapDispatchToProps = (dispatch) => {
   return {
     togglePlay: (isPlaying) => {
@@ -72,5 +59,4 @@ const mapDispatchToProps = (dispatch) => {
     }
   };
 };
-
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
