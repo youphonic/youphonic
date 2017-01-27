0457e824fd61af2b350247c2a8fc6e405194dce7
Ian Munro <ianmunrobot@gmail.com>
2017-01-26 19:30:08 -0500

    Update RightMenu.js

--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -41,8 +41,6 @@ function RightMenu (props) {
 					if (props.isPlaying) {
 						props.togglePlay(props.isPlaying);
 					} else {
-						props.myP5.loop();
-						props.myP5.noLoop();
 					}
 				}}/>
       <MenuItem primaryText="Triangle" />
