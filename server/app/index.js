fae3e11ff29472d26b33f0ab827eaf0c8198d664
David Rhodes <rhodesdav@gmail.com>
2017-01-28 14:59:07 -0500

    server firing

--- server/app/index.js
+++ server/app/index.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
 const volleyball = require('volleyball')
+const passport = require('passport')
+const cookies = require('cookie-session')
+const bodyParser = require('body-parser')
 
 const app = express();
 module.exports = app;
@@ -9,9 +12,26 @@ const staticMiddleware = require('./static.middleware');
 
 app.set('port', (process.env.PORT || 1337));
 
-app.use(volleyball)
+// dev environment logger
+const logger = volleyball.custom({ debug: true })
+app.use(logger)
+
+//store entire session on a cookie
+app.use(cookies({
+	name: 'session',
+	keys: [process.env.SESSION_SECRET || 'an insecure secret key']  //DR: where is process.env coming from? I know its a node variable but how do we set session secret on it?
+}))
+
+//body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+//authentication middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(staticMiddleware);
+
 
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', '..', 'src', 'index.html'));
