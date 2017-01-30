fac03a49bfe0ea391b0bd7beedae33ba05fafc0c
Ian Munro <ianmunrobot@gmail.com>
2017-01-30 18:21:17 -0500

    troubleshooting bounce physics

--- src/chunks/Chunk.js
+++ src/chunks/Chunk.js
@@ -1,16 +1,14 @@
-// import { ourP5 } from '../components/P5Wrapper'
-
-import {bounce} from './utils'
+import {bounce, reBounce} from './utils'
 
 let idCount = 1;
 
-const initialDirection = {x: 0, y: 0}
+// const initialDirection = new Point(0, 0)
 
 export default class Chunk {
-  constructor(x, y, direction = initialDirection) {
+  constructor(x, y, direction) {
     this.position = [x, y]
 	  this.id = idCount++;
-    this.direction = direction;
+    this.direction = direction
     // this is hacky for now - will be tied to Tone events eventually?
     this.color = [255, 255, 255]
     this.hitColor = [225, 139, 115];
@@ -25,11 +23,18 @@ export default class Chunk {
   update() {
     if (this.isMoving) {
       this.path.position = this.path.position.add(this.direction)
-      console.log(Math.floor(this.path.position.x), Math.floor(this.path.position.y));
+
+      // console.log(Math.floor(this.path.position.x), Math.floor(this.path.position.y));
     }
   }
 
   respondToHit(hitter) {
-    this.direction.angle -= bounce(this.path.position, hitter.path.position, this.direction)
+    // this.direction.angle -= bounce(this.path.position, hitter.path.position, this.direction)
+    // console.log('before', this.direction);
+    // console.log('before', hitter.direction);
+    reBounce(this, hitter)
+    console.log(this.direction, hitter.direction);
+    console.log(this.path, hitter.path);
+    // this.path.position.x -=100;
   }
 }
--- src/chunks/utils.js
+++ src/chunks/utils.js
@@ -6,3 +6,23 @@ export const bounce = function(point1, point2, vector) {
         // console.log(Math.atan2(yDiff, xDiff) * (180 / Math.PI);
   return Math.atan2(yDiff, xDiff) * (180 / Math.PI)
 }
+
+export const reBounce = function(obj1, obj2, vector) {
+  let point1 = obj1.path.position;
+  let point2 = obj2.path.position;
+  console.log(point1);
+  // let normal = point1.getDirectedAngle(point2) + 90;
+  // let dotProd = point1.dot(point2);
+
+  let dx = point1.x - point2.x
+  let dy = point1.y - point2.y
+  let distance = Math.hypot(dx, dy);
+  let tangent = Math.atan2(dy, dx)
+  point1.angle = 2 * tangent - point1.angle;
+  point2.angle = 2 * tangent - point2.angle;
+  let temp = obj1.direction
+  obj1.direction = obj2.direction
+  obj2.direction = temp
+  console.log('1', obj1.direction);
+  console.log('2', obj2.direction);
+}
