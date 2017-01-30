f23b8c9654342c8aebedf455c64a2758c928c1c5
bsaphier <b.saphier@gmail.com>
2017-01-30 14:41:24 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,5 @@ var Play = require('./api/plays/play.model');
 var User = require('./api/users/user.model');
 
 User.hasMany(Play, {foreignKey: 'player_id'});
-// Play.belongsTo(User);
 
 module.exports = {Play, User};
