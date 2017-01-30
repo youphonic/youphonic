7b99d1719aa91ea9ecfdb96130bacd235b041693
Ian Munro <ianmunrobot@gmail.com>
2017-01-30 16:27:52 -0500

    updated paperseed

--- src/redux/paperjsSeed.js
+++ src/redux/paperjsSeed.js
@@ -13,35 +13,17 @@ export default function () {
   let noMotion = new Point(0, 0)
 
   const circleSeed = [
-<<<<<<< HEAD
-    new Circle(center.x + xDiff, center.y + yDiff, radius),
-    new Circle(center.x - xDiff, center.y + yDiff, radius),
-    new Circle(center.x + xDiff, center.y - yDiff, radius),
-    new Circle(center.x - xDiff, center.y - yDiff, radius),
-  ];
-=======
     new Circle(center.x + xDiff, center.y + yDiff, radius, noMotion),
     new Circle(center.x - xDiff, center.y + yDiff, radius, noMotion),
     new Circle(center.x + xDiff, center.y - yDiff, radius, noMotion),
     new Circle(center.x - xDiff, center.y - yDiff, radius, noMotion),
   ]
->>>>>>> 0169fc6f51007dea7d09ff9cb585899c1361e913
 
   circleSeed.forEach((circle, index) => {
     circle.frequency = 100 * (index + 2);
     store.dispatch(addChunk(circle));
   });
 
-<<<<<<< HEAD
-  let bounceCircleMotion = {
-    x: 2,
-    y: 0
-  };
-
-  const bounceCircle = new Circle(center.x, center.y - yDiff + radius, radius / 2, bounceCircleMotion);
-  store.dispatch(addChunk(bounceCircle));
-}
-=======
   let bounceCircleMotion = new Point(2, 0)
   const bounceCircle = new Circle(center.x, center.y - yDiff + (radius * 1.414), radius, bounceCircleMotion)
   store.dispatch(addChunk(bounceCircle))
@@ -52,4 +34,3 @@ export default function () {
   // store.dispatch(addChunk(bounceCircle2))
 
 }
->>>>>>> 0169fc6f51007dea7d09ff9cb585899c1361e913
