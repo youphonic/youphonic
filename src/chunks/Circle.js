c570b69c7360e7946288877b9b9dd307ec40e1de
Ian Munro <ianmunrobot@gmail.com>
2017-01-31 15:00:21 -0500

    added color option

--- src/chunks/Circle.js
+++ src/chunks/Circle.js
@@ -3,13 +3,13 @@ import paper from 'paper'
 
 
 export default class Circle extends Chunk {
-  constructor(x, y, radius, direction) {
-    super(direction);
+  constructor(x, y, radius, direction, color = 'white') {
+    super(direction, color);
     this.radius = radius;
     this.path = new Path.Circle({
       center: [x, y],
       radius: radius,
-      fillColor: 'white',
+      fillColor: color,
     })
   }
 }
