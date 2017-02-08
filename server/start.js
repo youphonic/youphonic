8c95b38b5b70625c43ba92678995e7d069bede08
Ian Munro <ianmunrobot@gmail.com>
2017-02-07 22:39:11 -0500

    imported package

--- server/start.js
+++ server/start.js
@@ -1,11 +1,13 @@
 require('babel-register');
+const pkg = require('../index.js');
 
 const app = require('./app');
 const db = require('./_db');
 
+
 app.listen(app.get('port'), (err) => {
 	if (err) throw err;
-  console.log(`server listening on port ${app.get('port')}`);
+  console.log(`server for ${pkg.name} listening on port ${app.get('port')}`);
 	db.sync() //{force: true}
 	.then( () => {
 		console.log('...and postgres server is connected')
