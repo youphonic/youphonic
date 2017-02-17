d6d3924fca92d11f46b35311e70b488282161533
Benjamin Saphier <b.saphier@gmail.com>
2017-02-16 19:16:44 -0500

    dev page looking good, need to add links

--- src/components/DevInfo.js
+++ src/components/DevInfo.js
@@ -1,9 +1,9 @@
 import {
-  List,
   Paper,
   Dialog,
-  FontIcon,
-  ListItem,
+  GridList,
+  GridTile,
+  IconButton,
   FlatButton
 } from 'material-ui';
 import React from 'react';
@@ -12,61 +12,137 @@ import colors from '../colors';
 import devData from '../developerInfo';
 
 const styles = {
-  dev: {
+  root: {
+    overflow: 'visible'
+  },
+  list: {
+    paddingRight: 10,
+    overflow: 'visible'
+  },
+  paper: {
     margin: 5,
     width: '100%',
+    textAlign: 'left',
+    overflow: 'visible',
     display: 'inline-block',
-    color: colors.puertoRico,
-    boxShadow: 'rgba(18, 94, 104, 0.156863) 0px 3px 10px, rgba(18, 94, 104, 0.227451) 0px 3px 10px'
+    boxShadow: 'rgba(63, 35, 95, 0.156863) 0px 3px 10px, rgba(63, 35, 95, 0.227451) 0px 3px 10px'
+  },
+  headShotImage: {
+    margin: 25,
+    float: 'left',
+    textAlign: 'center',
+    backgroundColor: colors.smokeOnTheWater,
+    boxShadow: 'rgba(18, 94, 104, 0.117647) 0px 1px 6px, rgba(18, 94, 104, 0.117647) 0px 1px 4px'
+  },
+  image: {
+    margin: 'auto',
+    display: 'flex'
   },
   content: {
-    margin: 15,
-    padding: 5,
+    width: 'auto',
     height: 'inherit',
-    textAlign: 'left',
+    overflow: 'visible',
     color: colors.blueStone,
+  },
+  icon: {
+    color: colors.smokeOnTheWater
+  },
+  toolTip: {
+    color: colors.puertoRico
   }
 };
 
-const DevData = ({ name, gitHubLink, image }) => (
-  <ListItem
-    primaryText={name}
-    style={styles.content}
-    secondaryText={
-      <span>
-        <a href={gitHubLink}>{ gitHubLink }</a>
-        <FontIcon className="fa fa-github" />
-      </span>
-    }
-  />
+const DevData = ({
+  name,
+  image,
+  gitHubLink,
+  linkedinLink
+}) => (
+  <div>
+    <Paper style={styles.paper} zDepth={2}>
+      <GridTile rows={1}>
+        <Paper style={styles.headShotImage}>
+          <img
+            src={image}
+            alt={name}
+            height="138"
+            style={styles.image}
+          />
+        </Paper>
+        <GridTile style={styles.content}>
+          <h3 style={{marginTop: '8%'}}>{name}</h3>
+          <div>
+            <a
+              target="_blank"
+              href={gitHubLink}
+              rel="noopener noreferrer"
+            >
+              <IconButton
+                iconStyle={styles.icon}
+                tooltip={`${name}'s github`}
+                tooltipStyles={styles.toolTip}
+                tooltipPosition="bottom-center"
+                iconClassName="fa fa-github-square"
+              />
+            </a>
+            <a
+              target="_blank"
+              href={gitHubLink}
+              rel="noopener noreferrer"
+            >
+              <IconButton
+                iconStyle={styles.icon}
+                tooltip={`${name}'s linkedin`}
+                tooltipStyles={styles.toolTip}
+                tooltipPosition="bottom-center"
+                iconClassName="fa fa-linkedin-square"
+              />
+            </a>
+            {/* <a href={gitHubLink} target="_blank">
+              <IconButton
+                tooltip={`${name}'s github`}
+                iconClassName="fa fa-github"
+                tooltipPosition="top-left"
+              />
+            </a> */}
+          </div>
+        </GridTile>
+      </GridTile>
+    </Paper>
+  </div>
 );
 
-const devInfo = devData.map(dev =>
-  <DevData
-    name={dev.name}
-    key={dev.gitHubLink}
-    gitHubLink={dev.gitHubLink}
-    image={dev.image}
-  />
-);
+const DevInfo = ({ open, toggle }) => {
+  const devInfo = devData.map(dev =>
+    <DevData
+      name={dev.name}
+      image={dev.image}
+      key={dev.gitHubLink}
+      gitHubLink={dev.gitHubLink}
+      linkedinLink={dev.linkedinLink}
+    />
+  );
 
-const DevInfo = ({ open, toggle }) => (
-  <Dialog
-    style={styles.dev}
-    title="Develepor Information"
-    actions={
-      <FlatButton
-        label="Close"
-        onTouchTap={toggle}
-      />
-    }
-    modal={true}
-    open={open}
-  >
-    <List>
-      { devInfo }
-    </List>
-  </Dialog>
-);
+  return (
+    <Dialog
+      style={styles.root}
+      title="Developer Information"
+      actions={
+        <FlatButton label="Close" onTouchTap={toggle} />
+      }
+      modal={true}
+      open={open}
+    >
+      <GridList
+        cols={2}
+        padding={10}
+        cellHeight="auto"
+        style={styles.list}
+      >
+        { devInfo }
+      </GridList>
+    </Dialog>
+  );
+};
 
 export default DevInfo;
--- src/developerInfo/index.js
+++ src/developerInfo/index.js
@@ -2,22 +2,26 @@ const devData = [
   {
     name: 'Ian Munro',
     gitHubLink: 'https://github.com/ianmunrobot',
-    image: '../../public/headshots/ianMunro_HS.jpg'
+    image: '../../public/headshots/ianMunro_HS.jpg',
+    linkedinLink: ''
   },
   {
     name: 'David Rhodes',
     gitHubLink: 'https://github.com/dfrho',
-    image: '../../public/headshots/davidRhodes_HS.jpg'
+    image: '../../public/headshots/davidRhodes_HS.jpg',
+    linkedinLink: ''
   },
   {
     name: 'Ben Saphier',
     gitHubLink: 'https://github.com/bsaphier',
-    image: '../../public/headshots/benSaphier_HS.jpg'
+    image: '../../public/headshots/benSaphier_HS.jpg',
+    linkedinLink: ''
   },
   {
     name: 'Robbyn Tongue',
     gitHubLink: 'https://github.com/RTongue',
-    image: '../../public/headshots/robbynTongue_HS.jpg'
+    image: '../../public/headshots/robbynTongue_HS.jpg',
+    linkedinLink: ''
   }
 ];
 
